2acfd1f79c007a225c5f8cff4c00234e3575c071
Flavia <otrabalho67@gmail.com>
2025-04-25 16:10:29 -0300

    commit

---
+++ Assets/teste.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teste : MonoBehaviour
+{
+    decimal alturaJogador;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
