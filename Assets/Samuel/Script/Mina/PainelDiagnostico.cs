8862e072025ca75ce8786f9e2a1e48c7a4b06c8e
samvaladares <61877279+samvaladares@users.noreply.github.com>
2025-06-27 00:50:55 -0300

    Resolvendo bug

--- Assets/Samuel/Script/Mina/PainelDiagnostico.cs
+++ Assets/Samuel/Script/Mina/PainelDiagnostico.cs
@@ -6,126 +6,6 @@ using UnityEngine.UI;
 
 public class PainelDiagnostico : MonoBehaviour
 {
-    //[Header("UI")]
-    //public GameObject painel;
-    //public Text textoProblemas;
-    //public Transform listaChecklist;
-    //public GameObject itemChecklistPrefab;
-
-    //[Header("Config")]
-    //public KeyCode teclaFechar = KeyCode.Q;
-
-    //[Header("Puzzles")]
-    //public GameObject puzzleDrenagemPrefab; // Adicione esta linha
-
-    //[HideInInspector] public bool painelAtivo = false;
-    //private Barragem barragemAtual;
-
-    //void Update()
-    //{
-    //    if (painelAtivo && Input.GetKeyDown(teclaFechar))
-    //    {
-    //        FecharPainel();
-    //    }
-    //}
-
-    //public void AbrirPainel(Barragem barragem)
-    //{
-    //    barragemAtual = barragem;
-    //    painelAtivo = true;
-    //    painel.SetActive(true);
-    //    Time.timeScale = 0;
-
-    //    foreach (Transform child in listaChecklist)
-    //    {
-    //        Destroy(child.gameObject);
-    //    }
-
-    //    textoProblemas.text = "DIAGNÓSTICO DA BARRAGEM:\n";
-
-    //    foreach (var problema in barragem.problemas)
-    //    {
-    //        textoProblemas.text += $"• {problema.descricao}\n";
-
-    //        var item = Instantiate(itemChecklistPrefab, listaChecklist);
-    //        var toggle = item.GetComponent<Toggle>();
-    //        var text = item.GetComponentInChildren<Text>();
-
-    //        text.text = $"Resolver: {problema.descricao}";
-    //        toggle.isOn = problema.resolvido;
-
-    //        toggle.onValueChanged.AddListener((valor) => {
-    //            if (valor && !problema.resolvido)
-    //            {
-    //                toggle.isOn = false;
-    //               IniciarPuzzle(problema);
-    //            }
-    //            else
-    //            {
-    //                problema.resolvido = valor;
-    //                VerificarSolucao();
-    //            }
-    //        });
-    //    }
-    //}
-
-    //private void IniciarPuzzle(Barragem.Problema problema)
-    //{
-
-    //    FecharPainel();
-
-    //    if (problema.descricao.Contains("Nível de água"))
-    //    {
-    //        if (puzzleDrenagemPrefab == null)
-    //        {
-    //            Debug.LogError("puzzleDrenagemPrefab não está atribuído!");
-    //            return;
-    //        }
-
-    //        GameObject puzzleObj = Instantiate(puzzleDrenagemPrefab);
-    //        PuzzleManager puzzleManager = puzzleObj.GetComponentInChildren<PuzzleManager>();
-
-    //        if (puzzleManager == null)
-    //        {
-    //            Debug.LogError("PuzzleManager não encontrado!");
-    //            return;
-    //        }
-
-    //        // Usa o CoroutineManager para iniciar a Coroutine
-    //        CoroutineM.Instance.StartCoroutine(VerificarPuzzleCompleto(puzzleManager, problema));
-    //    }
-
-    //}
-
-    //void VerificarSolucao()
-    //{
-    //    if (barragemAtual.problemas.All(p => p.resolvido))
-    //    {
-    //        textoProblemas.text += "\nTODOS OS PROBLEMAS RESOLVIDOS!";
-    //    }
-    //}
-
-    //private IEnumerator VerificarPuzzleCompleto(PuzzleManager puzzleManager, Barragem.Problema problema)
-    //{
-    //    // Espera até o puzzle ser resolvido
-    //    while (!puzzleManager.puzzleCompleto)
-    //    {
-    //        yield return null;
-    //    }
-
-    //    // Quando resolvido, marca o problema como resolvido e reabre o painel
-    //    problema.resolvido = true;
-    //    AbrirPainel(barragemAtual);
-    //}
-
-    //public void FecharPainel()
-    //{
-    //    painelAtivo = false;
-    //    painel.SetActive(false);
-    //    Time.timeScale = 1;
-    //}
-
-
     [Header("UI")]
     public GameObject painel;
     public Text textoProblemas;
@@ -136,25 +16,25 @@ public class PainelDiagnostico : MonoBehaviour
     public KeyCode teclaFechar = KeyCode.Q;
 
     [Header("Puzzles")]
-    public List<GameObject> puzzlesPrefabs; // Lista de prefabs de puzzles (arraste no Inspector)
-    public GameObject puzzleAtual; // Referência ao puzzle instanciado
+    public List<GameObject> puzzlesPrefabs;
+    public GameObject puzzleAtual;
 
     [HideInInspector] public bool painelAtivo = false;
     private Barragem barragemAtual;
 
 
 
-    public static PainelDiagnostico instance; // Singleton público
+    public static PainelDiagnostico instance;
 
     void Awake()
     {
         if (instance == null)
         {
-            instance = this; // Define a instância única
+            instance = this;
         }
         else
         {
-            Destroy(gameObject); // Evita duplicatas
+            Destroy(gameObject);
         }
     }
     void Update()
@@ -172,7 +52,6 @@ public class PainelDiagnostico : MonoBehaviour
         painel.SetActive(true);
         Time.timeScale = 0;
 
-        // Limpa a lista anterior
         foreach (Transform child in listaChecklist)
         {
             Destroy(child.gameObject);
@@ -180,7 +59,6 @@ public class PainelDiagnostico : MonoBehaviour
 
         textoProblemas.text = "DIAGNÓSTICO DA BARRAGEM:\n";
 
-        // Cria os itens da checklist
         for (int i = 0; i < barragem.problemas.Count; i++)
         {
             var problema = barragem.problemas[i];
@@ -193,12 +71,12 @@ public class PainelDiagnostico : MonoBehaviour
             text.text = $"Resolver: {problema.descricao}";
             toggle.isOn = problema.resolvido;
 
-            int index = i; // Captura o índice para o closure
+            int index = i;
             toggle.onValueChanged.AddListener((valor) => {
                 if (valor && !problema.resolvido)
                 {
                     toggle.isOn = false;
-                    IniciarPuzzle(index); // Passa o índice do problema
+                    IniciarPuzzle(index);
                 }
                 else
                 {
@@ -222,7 +100,7 @@ public class PainelDiagnostico : MonoBehaviour
         }
 
         VerificarSolucao();
-        AbrirPainel(barragemAtual); // Reabre o painel
+        AbrirPainel(barragemAtual); 
     }
     private void IniciarPuzzle(int indexProblema)
     {
@@ -241,12 +119,11 @@ public class PainelDiagnostico : MonoBehaviour
 
         if (puzzlePiezometro != null)
         {
-            // Configura o callback DIRETAMENTE (sem condições extras)
+            
             puzzlePiezometro.onPuzzleComplete = () =>
             {
-                Debug.Log("Callback do puzzle executado!"); // Novo log
+                Debug.Log("Callback do puzzle executado!"); 
                 barragemAtual.problemas[indexProblema].resolvido = true;
-                CameraManager.Instance.VoltarAoJogador();
                 Destroy(puzzleAtual);
                 AbrirPainel(barragemAtual);
             };
